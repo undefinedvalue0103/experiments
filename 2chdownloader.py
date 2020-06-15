d144c86c0a6a226a169c7961d3bf601163640f63
hkc <starbfly0915@gmail.com>
2020-06-16 00:03:59 +0300

    More detailed progress and also CRLF->LF

--- 2chdownloader.py
+++ 2chdownloader.py
@@ -3,33 +3,47 @@
 Данная утилита предназначена для скачивания файлов с имиджборды 2ch.hk.
 
 Created by https://github.com/kiriharu
-Re-writed by https://github.com/undefinedvalue0103
+Re-writen by https://github.com/hatkidchan
 """
 import requests
 import traceback
 import re
+import os
 
-def download_file(fileobject: dict):
+IGNORE_TYPES = (100,)  # 100=sticker
+
+
+def mkdir(path: str):
+    if not os.path.exists(path):
+        os.mkdir(path)
+        print("[I] mkdir %r" % path)
+    else:
+        print("[I] directory %r already exists" % path)
+
+def download_file(fileobject: dict, out_folder: str=".", status: str=None):
     """
     Download file. Parameters given in file object
     @param fileobject: dict{path,fullname,md5}
+    @param out_folder: str
+    @param status: str
     """
     url = "https://2ch.hk" + fileobject["path"]
     fallback = fileobject["name"]
     name = fileobject["md5"] + " " + fileobject.get("fullname", fallback)
+    path = os.path.join(out_folder, name)
+    status = ("[%s]" % status) if status else ""
     try:
-        with open(name, "wb") as fd:
+        with open(path, "wb") as fd:
             with requests.get(url, stream=True) as rq:
                 expected_size = int(rq.headers.get("Content-Length", 1))
                 downloaded = 0
-                for chunk in rq.iter_content(chunk_size=8192):
+                for chunk in rq.iter_content(chunk_size=16384):
                     downloaded += len(chunk)
                     progress = downloaded / expected_size
-                    width = int(progress * 10)
-                    line = "[I] %35s: [%s%s] %7.3f%%" % (fileobject["path"],
-                                                     "=" * width,
-                                                     " " * (10 - width),
-                                                     progress * 100)
+                    line = "[I] %35s: [%-20s] %7.3f%% %s" % (fileobject["path"],
+                                                     "=" * int(progress * 20),
+                                                     progress * 100,
+                                                     status)
     
                     print(line, end="\r", flush=1)
                     fd.write(chunk)
@@ -41,62 +55,71 @@ def download_file(fileobject: dict):
     else:
         print("")
 
-def iter_files(board: str, thread: int=None, status:str=None):
+def iter_files(board: str, thread: int):
     """
     Yields files from thread
-    @param board: str, board letters (example, 'b')
-    @param thread: int/None, thread ID. If ommited, all threads given.
-    @param status: str/None, status string. used for progress
+    @param board: str, board letters (example, "b")
 
-    @returns generator(fileobject:dict)
+    @return generator(fileobject:dict, tuple(i, n_posts), tuple(j, n_files))
     """
-    if thread is not None:
-        thread_url = f"https://2ch.hk/{board}/res/{thread}.json"
-        print("[I] Getting", thread_url, status or "")
-        data = requests.get(thread_url).json()
-        for thread in data["threads"]:
-            for post in thread["posts"]:
-                for fileobj in post["files"]:
-                    if fileobj["type"] != 100:
-                        yield fileobj
-    else:
-        for index in range(1, 10):
-            index = "index" if index == 1 else index
-            page_url = f"https://2ch.hk/{board}/{index}.json"
-            print("[I] Getting", page_url)
-            data = requests.get(page_url).json()
-            threads = len(data["threads"])
-            for i, thread in enumerate(data["threads"], 1):
-                thread_num = thread["thread_num"]
-                for fileobj in iter_files(board, thread_num, "%3d/%3d"%(i, threads)):
-                    yield fileobj
+    thread_url = f"https://2ch.hk/{board}/res/{thread}.json"
+    print("[I] Getting", thread_url)
+    data = requests.get(thread_url).json()
+    for thread in data["threads"]:
+        n_posts = len(thread["posts"])
+        for i, post in enumerate(thread["posts"], 1):
+            n_files = len(post["files"])
+            for j, fileobj in enumerate(post["files"], 1):
+                if fileobj["type"] not in IGNORE_TYPES:
+                    yield fileobj, (i, n_posts), (j, n_files)
+
+def iter_threads(board: str):
+    for index in range(1, 10):
+        index = "index" if index == 1 else index
+        page_url = f"https://2ch.hk/{board}/{index}.json"
+        print("[I] Getting", page_url)
+        data = requests.get(page_url).json()
+        threads = len(data["threads"])
+        for i, thread in enumerate(data["threads"], 1):
+            thread_num = thread["thread_num"]
+            yield thread_num, (i, threads)
 
 def main():
     print("Welcome to 2chdownloader!")
-    print("Type board name as 'name' for downloading entire board")
-    print("Type thread as 'board/thread' for downloading one thread")
-    print("Type 'exit' for exit")
+    print("Type board name as 'name' to download all posts from board")
+    print("Type thread as 'board/thread' to download one thread")
+    print("Type 'exit' to exit")
     while True:
         try:
             line = input("> ")
             if line == "exit":
                 return
             elif re.match(r"^(\w+)$", line):
-                for fileobj in iter_files(line):
-                    download_file(fileobj)
+                mkdir(line)
+                for thread, (i, threads) in iter_threads(line):
+                    out_path = '%s/%s' % (line, thread)
+                    mkdir(out_path)
+                    print("[I] Downloading %s (%d/%d)" % (thread, i, threads))
+                    for fileobj, a, b in iter_files(line, thread):
+                        status = '%3d/%3d:%d/%d' % (a + b)
+                        download_file(fileobj, out_path, status)
                 print("[I] Done")
             elif re.match(r"^(\w+)\/(\d+)$", line):
                 board, thread = line.split("/")
-                for fileobj in iter_files(board, int(thread)):
-                    download_file(fileobj)
+                mkdir(board)
+                out_path = os.path.join(board, thread)
+                mkdir(out_path)
+                for fileobj, a, b in iter_files(board, int(thread)):
+                    status = '%3d/%3d:%d/%d' % (a + b)
+                    download_file(fileobj, out_path, status)
                 print("[I] Done")
             else:
                 print("Something went wrong")
         except KeyboardInterrupt:
             print("[I] Type 'exit' for exit")
         except Exception as e:
-            print("[E]", e)
-        
+            for line in traceback.format_exc().split("\n"):
+                print("[E] %s" % line)
 
 if __name__ == '__main__':
     main()
